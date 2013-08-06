22603534152d3958a3e1ddc0a35e7b31bd278027
colinplusplus <colinwilliamsfoh@gmail.com>
2013-08-06 11:33:12 -0400

    first commit

---
+++ about.php
@@ -0,0 +1,21 @@
+<?php $thisPage="About Us"; ?>
+<?php include('header.php')?>
+
+
+<h1 style="color: #fff;">About Us</h1>
+<img src="img/header.png" style="width:100%; margin-bottom: 10px;"/>
+	<div class="well">
+		<img src="img/Toronto-20120825-00221.jpg" class="text-wrap"style="height:400px; vertical-align:middle;">
+		
+
+		<p>Jameson’s Inc. is a family owned, multi generational, full service construction company.  We are committed to fair pricing, quality workmanship and timely completion.  Jameson’s specializes in all phases of residential and commercial construction from landscaping to custom homes and from office painting to full multi level commercial builds.</p>
+		<p>The owner, James Ward, has over 40 years experience in custom woodwork and construction.  He started out as an Irish immigrant that moved to Durham Region at the young age of 21.  He and his new bride Anne moved to Canada to have an adventure. They never dreamed they would build a life and family of their own here.  Four kids and three grandchildren later, they are still in Durham Region and Jameson’s has come to be known as one of the most reliable and respectable family run construction companies in the GTA; with clients as far East as Cobourg, West as Waterloo and North as Collingwood.</p>
+		<p>James started Jameson’s in 1978 and when his two sons, Robert and James were old enough to swing a hammer, started working for the family business.  Now, both sons are construction professionals and there are custom homes, churches, retail centers, train stations, gas stations and centennial building restorations throughout the GTA that all have their handy work attached to them.
+		<p>Customer satisfaction is a high priority at Jameson’s.  You can rest assured your experience will be pleasant with the solid craftsmanship at a fair price.  We would not only be happy to provide you with picture proof, but we would try to customize our testimonial package to clients that have had similar work done.</p>
+		<p>We work according to Ontario laws and all employees are health and safety trained, have fall protection, WSIB and WHMIS training to ensure that everyone on site is working safely and that home or property owners will also be safe.</p>
+		<p>As a full service construction company, we are able to provide service at all levels from concept and design to construction and project management.
+			We hope to add you and your family to our growing list of happy clients that have become part of our extended family.  If you use us once, you’ll never use anyone else.</p>
+	</div>
+
+
+<?php include('footer.php')?>
